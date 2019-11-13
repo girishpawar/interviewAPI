a8c1f531de009235388e8d2266f468a2a40c30aa
Girish Pawar <girish.suresh.pawar@gmail.com>
2019-11-13 13:12:13 +0530

    Done changes in API Testing

--- app/controllers/content.controller.js
+++ app/controllers/content.controller.js
@@ -2,54 +2,78 @@ const Note = require('../models/content.model.js');
 
 // Create and Save a new Note
 exports.create = (req, res) => {
-    // Validate request
-    // if(!req.body.content) {
-    //     return res.status(400).send({
-    //         message: "Note content can not be empty"
-    //     });
-    // }
-
-    // Create a Note
-    const note = new Note({
-        title: req.body.title || "Untitled Note", 
-        category: req.body.category,
-        notes: req.body.notes
-    });
-
-    // Save Note in the database
-    note.save()
-    .then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
+    //Validate request
+    if(!req.body.title || !req.body.category || !req.body.notes) {
+        return res.status(400).send({
+            message: "Please provide notes data!", success:false, error: true
         });
-    });
+    }else{
+
+        // Create a Note
+        const note = new Note({
+            title: req.body.title || "Untitled Note", 
+            category: req.body.category,
+            notes: req.body.notes
+        });
+
+        // Save Note in the database
+        note.save()
+        .then(notedata => {
+            //res.send(data);
+            return res.status(200).send({
+                message: "Note added successfully!", success:true, error: false,data : notedata
+            });
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "An error occurred while creating the Note.", 
+                success:false, error: true
+            });
+        });
+    }
 };
 
 // Retrieve and return all notes from the database.
 exports.findAll = (req, res) => {
+    
     Note.find()
     .then(notes => {
-        res.send(notes);
+        //res.send(notes);
         console.log("Got all notes",notes);
+        return res.status(200).send({
+            message: "Get all notes.",
+            data : notes,
+            success:true,
+            error: false
+        });    
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while retrieving notes."
+            message: err.message || "Some error occurred while retrieving notes.",
+            success:false,
+            error: true    
         });
     });
 };
 
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
+
+
     Note.findById(req.params.noteId)
     .then(note => {
         if(!note) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "Note not found with id " + req.params.noteId,
+                success:false,
+                error: true    
             });            
+        }else{
+            return res.status(200).send({
+                message: "Note Found :" + req.params.noteId,
+                data:note,
+                success:true,
+                error: false    
+            });       
         }
-        res.send(note);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
@@ -64,56 +88,61 @@ exports.findOne = (req, res) => {
 
 // Update a note identified by the noteId in the request
 exports.update = (req, res) => {
-    // Validate Request
-    // if(!req.body.content) {
-    //     return res.status(400).send({
-    //         message: "Note content can not be empty"
-    //     });
-    // }
-
-    // Find note and update it with the request body
-    Note.findByIdAndUpdate(req.params.noteId, {
-        title: req.body.title || "Untitled Note",
-        category: req.body.category,
-        notes: req.body.notes
-    }, {new: true})
-    .then(note => {
-        if(!note) {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });
-        }
-        res.send(note);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });                
-        }
-        return res.status(500).send({
-            message: "Error updating note with id " + req.params.noteId
+     //Validate request
+     if(!req.body.title || !req.body.category || !req.body.notes ) {
+        return res.status(400).send({
+            message: "Please provide notes data!", success:false, error: true
         });
-    });
+    }else{
+        // Find note and update it with the request body
+        Note.findByIdAndUpdate(req.params.noteId, {
+            title: req.body.title || "Untitled Note",
+            category: req.body.category,
+            notes: req.body.notes
+        }, {new: true})
+        .then(note => {
+            if(!note) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId, success:false, error: true
+                });
+            }else{
+                return res.status(200).send({
+                    message: "Note updated successfully!", success:true, error: false, data:note
+                });
+            }
+            //res.send(note);
+        }).catch(err => {
+            if(err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId,success:false, error: true
+                });                
+            }
+            return res.status(500).send({
+                message: "Error updating note with id " + req.params.noteId,success:false, error: true
+            });
+        }); 
+    }
 };
 
 // Delete a note with the specified noteId in the request
 exports.delete = (req, res) => {
+
     Note.findByIdAndRemove(req.params.noteId)
     .then(note => {
         if(!note) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "Note not found with id " + req.params.noteId,success:false, error: true
             });
         }
-        res.send({message: "Note deleted successfully!"});
+        res.send({message: "Note deleted successfully!", success:true, error: false});
     }).catch(err => {
         if(err.kind === 'ObjectId' || err.name === 'NotFound') {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "Note not found with id " + req.params.noteId,success:false, error: true
             });                
         }
         return res.status(500).send({
-            message: "Could not delete note with id " + req.params.noteId
+            message: "Could not delete note with id " + req.params.noteId,success:false, error: true
         });
     });
 };
--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -5,7 +5,7 @@ exports.create = (req, res) => {
     //Validate request
     if(!req.body.email || !req.body.password) {
         return res.status(400).send({
-            message: "User content can not be empty!"
+            message: "User content can not be empty!", success:false, error: true
         });
     }
     console.log("Request data:"+JSON.stringify(req.body));
@@ -19,25 +19,18 @@ exports.create = (req, res) => {
         if (err) {
             res.send(err)
         }else {
-            if (user) {
+            if (!user) {
                 newUser.save(function (err) {
                     if (err) {
                         res.send(err)
                     } else {
-                        res.send({ statusMessage: "User updated successfully", status: "Success", data: newUser });
-                    }
-                });
-
-            } else {
-                
-                newUser.save(function (err) {
-                    if (err) {
-                        res.send(err)
-                    } else {
-                        res.send({ statusMessage: "User added successfully", status: "Success", data: newUser });
+                        res.send({ statusMessage: "User added successfully!", success:false,error: true, data: newUser });
                     }
                 });
                 // }
+            }else{
+               
+                res.send({ statusMessage: "User already exist!", success:false, error: true, data: newUser });
             }
         }
     })
@@ -46,32 +39,50 @@ exports.create = (req, res) => {
 
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
-    console.log("In single user find:"+req.body.email)
-    User.findOne({ email : req.body.email , password: req.body.password } )
-    .then(user => {
-        if(!user) {
-            return res.status(404).send({
-                message: "User not Found"
-            });            
-        }
 
-        if(user) {
-            return res.status(200).send({
-                message: "Logged in successfully!",
-                data : user
-            });            
-        }
-        res.send(user);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Incorrect Login Details"
-            });                
-        }
-        return res.status(500).send({
-            message: "Error retrieving User details "
+    //Validate request
+    if(!req.body.email || !req.body.password) {
+        return res.status(400).send({
+            message: "Please provide user details!", success:false, error: true
         });
-    });
+    }else{
+
+        console.log("In single user find:"+req.body.email)
+        User.findOne({ email : req.body.email , password: req.body.password } )
+        .then(user => {
+            if(!user) {
+                return res.status(404).send({
+                    message: "Incorrect user details!",
+                    success:false,
+                    error: true
+                });   
+                //res.send({ statusMessage: "User added successfully", status: "Success", data: newUser });         
+            }
+
+            if(user) {
+                return res.status(200).send({
+                    message: "Logged in successfully!",
+                    data : user,
+                    success:true,
+                    error: false
+                });            
+            }
+            res.send(user);
+        }).catch(err => {
+            if(err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Incorrect Login Details",
+                    success:false,
+                    error: true
+                });                
+            }
+            return res.status(500).send({
+                message: "Error retrieving User details ",
+                success:false,
+                error: true
+            });
+        });
+    }
 };
 
 
